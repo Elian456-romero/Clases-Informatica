59ff77e97c699f310bb525a189771dcb2df31536
Elian Romero <e12429@ism.edu.ec>
2026-04-14 12:36:11 -0500

    Actualización 1

---
+++ Actividad 2 (Variables).py
@@ -0,0 +1,12 @@
+first_name= "Elian"
+print("Primer nombre: ", first_name)
+print("Largo del primer nombre: ",len(first_name))
+print(type(first_name))
+
+#%%
+
+primer_nombre, segundo_nombre, pais, edad, estadocivil = "Elian", "Romero", "Ecuador", "17", "Soltero"
+print("Pais: ", pais)
+print("edad: ", edad)
+
+#%%
