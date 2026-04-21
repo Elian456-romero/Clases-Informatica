e996df63c295bfb35233f89474ba6f331d6558f7
Elian Romero <e12429@ism.edu.ec>
2026-04-21 10:57:10 -0500

    Actualiazción 1

---
+++ Actividad 4 (strings).py
@@ -0,0 +1,33 @@
+stringVariasLineas = """
+Elian Romero
+Carlos Oñate
+Quito Ecuador """
+
+print(stringVariasLineas)
+
+colegio = "ISM"
+longitud =(len(colegio))
+print(longitud)
+
+print(len(colegio))
+
+nombre = "Elian"
+apellido = "Romero"
+espacio= " "
+nombreCompleto= nombre + espacio + apellido
+print("mi nombre completo es: " + nombreCompleto)
+
+#multiplicar la variable las veces que desea
+print(nombreCompleto*4)
+
+#
+print("Python\nChallenge")
+
+print("Days\tTopics")
+print("nombre\tSema1\tSema2\tSema3")
+
+print("Simbolo(\\)")
+print("@")
+
+print(f"Mi nombre es {nombre} y mi apellido es {apellido}")
+
