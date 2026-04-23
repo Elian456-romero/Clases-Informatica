48bfde370c7a4c554984f4fba298b2d3fb10abd7
Elian Romero <e12429@ism.edu.ec>
2026-04-23 14:13:23 -0500

    Actualizacion 1

---
+++ Actividad 5 (cpndicionales).py
@@ -0,0 +1,21 @@
+b=3
+if b > 0:
+    print(f"{b} es un numero positivo")
+
+nota= int(input("Ingresa tu nota: ")) 
+
+if nota > 90:
+    print("Tu nota es A")
+else:
+    if nota > 80:
+        print("Tu nota es B")
+    else:
+        if nota > 70:
+            print("Tu nota es C")
+        else:
+            if nota > 50:
+                ("Tu nota es D")
+            else:
+                if nota < 49:
+                    print("Reprobado")
+
