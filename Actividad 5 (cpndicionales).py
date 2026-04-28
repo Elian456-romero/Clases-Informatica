bcae81aeaa880f13d2c05c6439349f82394f547c
Elian Romero <e12429@ism.edu.ec>
2026-04-28 10:58:15 -0500

    Actualizacion 7

--- Actividad 5 (cpndicionales).py
+++ Actividad 5 (cpndicionales).py
@@ -1,7 +1,7 @@
 b=3
 if b > 0:
     print(f"{b} es un numero positivo")
-
+8
 nota= int(input("Ingresa tu nota: ")) 
 
 if nota > 90:
@@ -19,3 +19,36 @@ else:
                 if nota < 49:
                     print("Reprobado")
 
+#%%
+
+print("Ingrese un número")
+numero = int(input())
+
+if numero == 0:
+    print("El número es cero")
+else:
+    if numero > 0:
+        if numero % 2 == 0:
+            print("El número es positivo y par")
+        else:
+            print("El número es positivo e impar")
+    else:
+        if numero % 2 == 0:
+            print("El número es negativo y par")
+        else:
+            print("El número es negativo e impar")
+print("Fin del programa")
+
+
+print ("Ingrese un número")
+b = int(input())
+if b > 0 and b % 2 == 0:
+    print("El número es positivo y par")
+elif b > 0 and b % 2 == 1:
+    print("El número es positivo e impar")
+elif b < 0 and b % 2 == 0:
+    print("El número es negativo y par")
+elif b < 0 and b % 2 == 1:
+    print("El número es negativo e impar")
+else:
+    print("El numero es cero")
