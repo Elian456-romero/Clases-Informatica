df4c1e98c17071d849415f72630c26b1f6c00df2
Elian Romero <e12429@ism.edu.ec>
2026-05-12 12:31:42 -0500

    Actualizacion final

---
+++ Actividad 6 (ciclo while).py
@@ -0,0 +1,53 @@
+
+
+"""opcion = ""
+while opcion != "c":
+    print("Menú de opciones:")
+    print("a. Opción 1")
+    print("b. Opción 2")
+    print("c. Salir")
+    opcion = input("Seleccione una opción: ")       
+    print (opcion)
+    if opcion == "a":
+        print ("Hola Bienvenido")
+    elif opcion == "b":
+        print ("Estamos aprendiendo python")
+    elif opcion == "c":
+        print("Saliendo del programa...")
+    else:
+        print("Opcion no valida")
+
+numbers = [0,1,2,3,4,5]
+for number in numbers:
+    print (f"Su iterador es {number}")
+
+notas=[8,7,9,10]
+suma=0
+for nota in notas:
+    suma=suma+nota
+promedio=suma/len(notas)
+print(promedio)
+
+notas=[8,7,9,10]
+suma=0
+contador=0
+
+for nota in notas:
+    suma=suma+nota
+    contador+=1
+promedio=suma/contador
+print(f"El promedio es {promedio}")
+
+language= "P y t h o n"
+for letter in language:
+    print(letter)"""
+
+palabra = input ("Ingrese una palabra: ").lower()
+vocales = 0
+
+for letra in palabra:
+    if letra == "a" or letra == "e" or letra == "i" or letra == "o" or letra == "u":
+        vocales = vocales + 1
+print(f"La cantidad de vocales en la palabra es: {vocales}")
+print(f"La cantidad de consonantes en la palabra es: {len(palabra) - vocales}")
+print(f"El total de letras: {len(palabra)}")
